1dad2dd83d207c08623b8778716716ef6bfbb0d0
zhezh <zhezh.boy@gmail.com>
2017-06-08 13:26:03 +0800

    feat(($reflect)): add reflect demo

--- reflect/main.go
+++ reflect/main.go
@@ -1,1 +1,20 @@
-package reflect
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var name float64 = 3.4
+
+	t := reflect.TypeOf(name)
+	fmt.Println(t)
+	fmt.Println(t.String())
+
+	v := reflect.ValueOf(name)
+	fmt.Println(v)
+	fmt.Println(v.String())
+
+	v.Elem()
+}
