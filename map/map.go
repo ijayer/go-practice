a78864d5f37d01d6ab2b1f979b5c2dbcd009a40f
zhezh09 <boyzhz@163.com>
2017-07-11 22:34:22 +0800

    feat($map): add demo

---
+++ map/map.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	int_chan := make(chan int, 1)
+	string_chan := make(chan string, 1)
+	int_chan <- 1
+	string_chan <- "hello"
+	select {
+	case value := <-int_chan:
+		fmt.Println(value)
+	case value := <-string_chan:
+		time.Sleep(1 * time.Millisecond)
+		panic(value)
+	}
+}
