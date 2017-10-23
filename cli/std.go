e6ce08638929ec9c7c6d44fdc6b47813a7ce8e18
张哲 <zhezh.boy@gmail.com>
2017-10-23 14:49:42 +0800

    Add external command call demo

--- cli/std.go
+++ cli/std.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
-	"github.com/kataras/go-errors"
 	"os"
 	"strings"
 )
---
+++ cmd/cmd.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() {
+	cmd := "ls"
+	result, err := ExecCmd(cmd)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Result: %v\n", string(result))
+}
+
+// ExecCmd execute command on host and return the result to client.
+func ExecCmd(command string) ([]byte, error) {
+	cmd := exec.Command("/bin/sh", "-c", command)
+
+	stdout, err := cmd.StdoutPipe() // Obtain cmd output pipe
+	if err != nil {
+		log.Errorf("Error: obtain stdout pipe")
+		return nil, err
+	}
+
+	err = cmd.Start() // execute command
+	if err != nil {
+		log.Errorf("Error: execute command")
+		return nil, err
+	}
+
+	bytes, err := ioutil.ReadAll(stdout) // read result
+	if err != nil {
+		log.Errorf("Error: read result")
+		return nil, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Errorf("Error: wait to exit")
+		return nil, err
+	}
+
+	return bytes, nil
+}
