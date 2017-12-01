73897d5ae39e7ed0b40c9b5a59bb8a218e481e1d
张哲 <zhezh.boy@gmail.com>
2017-12-01 14:12:39 +0800

    update test demo

--- test/unit/main_test.go
+++ test/unit/main_test.go
@@ -4,33 +4,39 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	r := Add(1, 2)
-	if r != 3 {
-		t.Fail() // continue next testing
+	if r != 1 {
+		t.Fail() // continue next testing func
+	} else {
+		t.Log("add test is ok")
 	}
-	t.Log("add test is ok")
+	t.Log("mark Fail")
 }
 
 func TestSub(t *testing.T) {
 	r := Sub(1, 2)
-	if r != -1 {
-		t.FailNow() // current file testing end in here, and continue test next file testing
+	if r != 1 {
+		t.FailNow() // current file testing stopped, and continue next testing func
+	} else {
+		t.Log("sub test is ok")
 	}
-	t.Log("sub test is ok")
+	t.Log("mark FailNow")
 }
 
 func TestMul(t *testing.T) {
 	r := Mul(1, 2)
 	if r != 2 {
 		t.Fatal() // = t.Log + t.FailNow()
+	} else {
+		t.Log("mul test is ok")
 	}
-	t.Log("mul test is ok")
 }
 
 func TestDiv(t *testing.T) {
 	if r, err := Div(4, 2); err != nil || r != 2 { // usual case
 		t.Errorf("div test failed: %v\n", err)
+	} else {
+		t.Log("div test is ok")
 	}
-	t.Log("div test is ok")
 }
 
 /*
